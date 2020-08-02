673ee4ba9770326c6f95aa6fdb1c7746684a1c6d
kavindu <kavindu@wso2.com>
2020-08-02 21:40:46 +0530

    Change the compiler phases

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TaintAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TaintAnalyzer.java
@@ -62,6 +62,7 @@ import org.wso2.ballerinalang.compiler.tree.BLangVariable;
 import org.wso2.ballerinalang.compiler.tree.BLangWorker;
 import org.wso2.ballerinalang.compiler.tree.BLangXMLNS;
 import org.wso2.ballerinalang.compiler.tree.clauses.BLangInputClause;
+import org.wso2.ballerinalang.compiler.tree.clauses.BLangMatchClause;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangAccessExpression;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangAnnotAccessExpr;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangArrowFunction;
@@ -135,6 +136,7 @@ import org.wso2.ballerinalang.compiler.tree.statements.BLangForkJoin;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangIf;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangLock;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangMatch;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangMatchStatement;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangPanic;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangRecordDestructure;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangRecordVariableDef;
@@ -791,6 +793,22 @@ public class TaintAnalyzer extends BLangNodeVisitor {
             ifNode.elseStmt.accept(this);
         }
         overridingAnalysis = true;
+    }
+
+    @Override
+    public void visit(BLangMatchStatement matchStatement) {
+        matchStatement.expr.accept(this);
+        TaintedStatus observedTaintedStatusOfMatchExpr = getCurrentAnalysisState().taintedStatus;
+
+        for (BLangMatchClause matchClause : matchStatement.matchClauses) {
+            getCurrentAnalysisState().taintedStatus = observedTaintedStatusOfMatchExpr;
+            matchClause.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(BLangMatchClause matchClause) {
+        matchClause.blockStmt.accept(this);
     }
 
     @Override
